58ba449d7a204deaa3847192246ddef2546fa739
Matias <matias.marcelo.calvo@gmail.com>
2021-03-22 01:07:55 -0300

    App component changes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,28 @@
+// @Vendors
 import React from 'react';
 import { connect } from 'react-redux';
-import logo from './logo.svg';
+
+// @Components
+import PostsList from './components/layout/PostsList/PostsList';
+import PostDetail from './components/layout/PostDetail/PostDetail'
+
+// @Styles
 import './App.css';
 
-// @Actions
-import RedditActions from './redux/RedditRedux';
-
-function App({ fetchTop }) {
-  React.useEffect(() => {
-    fetchTop();
-  }, [fetchTop]);
-
+function App({ selectedPost }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <main className="App">
+      <PostsList />
+
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </main>
   );
 }
 
-const mapDispatchToProps = {
-  fetchTop: RedditActions.fetchTopRequest
+const mapStateToProps = ({ reddit }) => {
+  return {
+    selectedPost: reddit.selectedPost
+  };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, null)(App);
