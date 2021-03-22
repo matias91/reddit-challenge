ddeeda4030dd00d0624211bf98584d304969b2fc
Matias <matias.marcelo.calvo@gmail.com>
2021-03-22 01:06:27 -0300

    PostsList component created

---
+++ src/components/layout/PostsList/PostsList.js
@@ -0,0 +1,88 @@
+// @Vendor
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+// @Components
+import Post from '../../commons/Post/Post';
+
+// @Actions
+import RedditActions from '../../../redux/RedditRedux';
+
+// @Styles
+import './PostsList.css';
+
+function PostsList({ after, before, count, dissmissAllPosts, fetchTop, fetching, posts }) {
+  React.useEffect(() => {
+    fetchTop();
+  }, [fetchTop]);
+
+  const nextPage = () => fetchTop(after);
+
+  const prevPage = () => fetchTop(null, before);
+
+  const renderPosts = () => {
+    return posts.map((post, index) =>
+      <Post
+        index={index}
+        key={index}
+        post={post.data}
+      />
+    )
+  }
+
+  const postsArray = posts && renderPosts();
+
+  return (
+    <aside className='PostsList'>
+      <header className='PostsList-header'>
+        <button disabled={count === 50 || !posts.length} onClick={prevPage} style={{ borderRadius: 25, backgroundColor: '#FFF', border: 0, height: 50, width: 50, cursor: 'pointer' }}>
+          <p>Prev</p>
+        </button>
+        <h2>Reddit Posts</h2>
+        <button disabled={!posts.length} onClick={nextPage} style={{ borderRadius: 25, backgroundColor: '#FFF', border: 0, height: 50, width: 50, cursor: 'pointer' }}>
+          <p>Next</p>
+        </button>
+      </header>
+      <ul className='PostsList-list'>
+        {
+          fetching &&
+          <div style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <div className="loader" />
+          </div>
+        }
+        {postsArray}
+      </ul>
+      <footer className='PostsList-footer' onClick={dissmissAllPosts}>
+        Dismiss All
+      </footer>
+    </aside >
+  );
+}
+
+PostsList.propTypes = {
+  after: PropTypes.string,
+  before: PropTypes.string,
+  count: PropTypes.number,
+  dissmissAllPosts: PropTypes.func,
+  fetchTop: PropTypes.func,
+  fetching: PropTypes.bool,
+  posts: PropTypes.array
+}
+
+const mapStateToProps = ({ reddit }) => {
+  return {
+    after: reddit.after,
+    before: reddit.before,
+    count: reddit.count,
+    fetching: reddit.fetching,
+    posts: reddit.posts
+  };
+};
+
+const mapDispatchToProps = {
+  dissmissAllPosts: RedditActions.dissmissAllPosts,
+  fetchTop: RedditActions.fetchTopRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsList);
