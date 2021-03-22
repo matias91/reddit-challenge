ef73857451db0a69c46a74e6477a67ecc7b37eb8
Matias <matias.marcelo.calvo@gmail.com>
2021-03-22 01:08:21 -0300

    Reducer actions added

--- src/redux/RedditRedux.js
+++ src/redux/RedditRedux.js
@@ -6,9 +6,15 @@ import Immutable from 'seamless-immutable';
 
 const { Types, Creators } = createActions({
   // FETCH TOP
-  fetchTopRequest: null,
-  fetchTopSuccess: ['response'],
+  fetchTopRequest: ['after', 'before'],
+  fetchTopSuccess: ['response', 'before'],
   fetchTopFailure: ['response'],
+  // SET SELECTED POST
+  setSelectedPost: ['selectedPost'],
+  // DISSMISS POST
+  dissmissPost: ['postIndex'],
+  // DISSMISS ALL POSTS
+  dissmissAllPosts: null,
 });
 
 export const RedditTypes = Types;
@@ -17,9 +23,13 @@ export default Creators;
 /* ------------- Initial State ------------- */
 
 export const INITIAL_STATE = Immutable({
+  after: null,
+  before: null,
+  count: 0,
   errorMessage: '',
-  feed: [],
   fetching: false,
+  posts: [],
+  selectedPost: null,
   showError: false,
 });
 
@@ -32,15 +42,25 @@ export const request = (state) =>
     showError: false
   });
 
+export const fetchTopRequest = (state) =>
+  state.merge({
+    errorMessage: '',
+    fetching: true,
+    posts: [],
+    showError: false
+  });
+
 export const success = (state, { response }) =>
   state.merge({
     fetching: false
   });
 
-export const fetchTopSuccess = (state, { response }) =>
+export const fetchTopSuccess = (state, { response, before }) =>
   state.merge({
     after: response.data.after,
-    feed: response.data.children,
+    before: response.data.before,
+    count: before ? state.count - 50 : state.count + 50,
+    posts: response.data.children,
     fetching: false
   });
 
@@ -51,11 +71,31 @@ export const failure = (state, { response }) =>
     showError: true
   });
 
+export const setSelectedPost = (state, { selectedPost }) =>
+  state.merge({
+    selectedPost
+  });
+
+export const dissmissPost = (state, { postIndex }) =>
+  state.merge({
+    posts: state.posts.filter((post, index) => index !== postIndex),
+    selectedPost: null
+  });
+
+export const dissmissAllPosts = (state) =>
+  state.merge(INITIAL_STATE);
+
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
   // FETCH TOP
-  [Types.FETCH_TOP_REQUEST]: request,
+  [Types.FETCH_TOP_REQUEST]: fetchTopRequest,
   [Types.FETCH_TOP_SUCCESS]: fetchTopSuccess,
   [Types.FETCH_TOP_FAILURE]: failure,
+  // SET SELECTED POST
+  [Types.SET_SELECTED_POST]: setSelectedPost,
+  // DISSMISS POST
+  [Types.DISSMISS_POST]: dissmissPost,
+  // DISSMISS ALL POSTS
+  [Types.DISSMISS_ALL_POSTS]: dissmissAllPosts,
 });
